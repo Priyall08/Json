214964345ab3aa8500eec591772de435fb9d6719
exploresystem_k <support@exploresystem.com>
2026-01-05 13:35:36 +0530

    Changed in index file to to handle both case

--- public/index.php
+++ public/index.php
@@ -1,6 +1,14 @@
 <?php
-require_once(__DIR__ . '/../app/controllers/AuthController.php');
-require_once(__DIR__ . '/../config/config.php');
+
+
+if (file_exists(__DIR__ . '/../app/controllers/AuthController.php')) {
+    require_once(__DIR__ . '/../app/controllers/AuthController.php');
+    require_once(__DIR__ . '/../config/config.php');
+} else {
+    require_once(__DIR__ . '/app/controllers/AuthController.php');
+    require_once(__DIR__ . '/config/config.php');
+}
+
 
 
 $action = $_GET['action'] ?? '';
